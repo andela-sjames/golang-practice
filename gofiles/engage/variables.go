fbd92a07172755637a9167e0db05c6cac6f173d3
andela-sjames <samuel.james@andela.com>
2016-10-07 13:38:48 +0100

    add variables and constant file

---
+++ gofiles/engage/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("here we go again")
+
+	var a string = "This is Andela"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d bool = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	short := "This is for short declaration."
+	fmt.Println(short)
+}
