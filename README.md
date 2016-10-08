2fc6f59d4779d3cb5d59d12fafa113493669ab56
andela-sjames <samuel.james@andela.com>
2016-10-08 09:47:07 +0100

    add map and readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.
+
+This repo contains examples on `golan` and some `hackerrank` exercises... 
+Want to take a peek?
+
+check out the `engage` folder.
---
+++ gofiles/engage/map.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("We have not even started yet")
+
+	// make(map[key-type]val-type)
+
+	m := make(map[string]int)
+
+	m["val1"] = 45
+	m["val2"] = 90
+
+	fmt.Println("map: ", m)
+
+	v1 := m["val1"]
+	fmt.Println("v1: ", v1)
+
+	fmt.Println("len: ", len(m))
+
+	delete(m, "val2")
+	fmt.Println("map: ", m)
+
+	_, prs := m["val2"]
+	fmt.Println("prs: ", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2} // create the map on the fly!
+	fmt.Println("map: ", n)
+}
