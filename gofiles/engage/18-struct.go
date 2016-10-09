59e7597585fe00f166aa0212f08cf4b634cedede
andela-sjames <samuel.james@andela.com>
2016-10-09 21:07:31 +0100

    add struct

---
+++ gofiles/engage/18-struct.go
@@ -0,0 +1,43 @@
+/*
+Go’s structs are typed collections of fields.
+They’re useful for grouping data together to form records.
+*/
+
+package main
+
+import "fmt"
+
+// This person struct type has name and age fields.
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	fmt.Println("let's go a little bit further...")
+
+	// This syntax creates a new struct.
+	fmt.Println(person{"Bob", 20})
+
+	// You can name the fields when initializing a struct.
+	fmt.Println(person{name: "Alice", age: 23})
+
+	// Omitted fields will be zero-valued.
+	fmt.Println(person{name: "Andela"})
+
+	// An & prefix yields a pointer to the struct.
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	// Access struct fields with a dot.
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	// You can also use dots with struct pointers - the pointers are automatically dereferenced.
+	sp := &s
+	fmt.Println(sp.age)
+
+	// Structs are mutable.
+	sp.age = 51
+	fmt.Println(sp.age)
+
+}
