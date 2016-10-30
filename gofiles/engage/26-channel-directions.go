a9f18bedf797895805b462da315af546c8a84463
andela-sjames <samuel.james@andela.com>
2016-10-30 22:08:06 +0100

    add introductory comment

--- gofiles/engage/26-channel-directions.go
+++ gofiles/engage/26-channel-directions.go
@@ -1,3 +1,9 @@
+/*
+When using channels as function parameters, you can specify
+if a channel is meant to only send or receive values.
+This specificity increases the type-safety of the program.
+*/
+
 package main
 
 import "fmt"
