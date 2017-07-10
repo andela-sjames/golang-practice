2486a7ee464d6c1d69c2d95cb675597914243117
andela-sjames <samuel.james@andela.com>
2017-07-10 15:59:45 +0100

    add timers

--- gofiles/engage/31-range-over-channels.go
+++ gofiles/engage/31-range-over-channels.go
@@ -7,11 +7,17 @@ import "fmt"
 func main() {
 	fmt.Println("here we go again, let's iterate over a go channel")
 
+	// We’ll iterate over 2 values in the queue channel.
+
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
 
 	close(queue)
+
+	// This range iterates over each element as it’s received from queue.
+	// Because we closed the channel above,
+	// the iteration terminates after receiving the 2 elements.
 
 	for elem := range queue {
 		fmt.Println(elem)
