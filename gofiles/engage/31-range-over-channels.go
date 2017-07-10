9a38b963f92ccdf7532318a22cdbab102a458a20
andela-sjames <samuel.james@andela.com>
2017-07-10 15:30:03 +0100

    add range over channels

--- gofiles/engage/31-range-over-channels.go
+++ gofiles/engage/31-range-over-channels.go
@@ -1,7 +1,19 @@
+//  Here we make attempt to iterate over values received from a channel.
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("here we go again")
+	fmt.Println("here we go again, let's iterate over a go channel")
+
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+
+	close(queue)
+
+	for elem := range queue {
+		fmt.Println(elem)
+	}
 }
