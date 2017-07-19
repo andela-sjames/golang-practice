391a064d4969b841592a429e0343a6b74f17fda8
andela-sjames <samuel.james@andela.com>
2017-07-19 13:26:52 +0100

    add hackerrank code compare triplet

--- gofiles/hackerrank/warm_up/compare_triplets.go
+++ gofiles/hackerrank/warm_up/compare_triplets.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	a, b := ReadArrays()
+	sa, sb := CompareIt(a, b)
+	fmt.Printf("%d %d", sa, sb)
+}
+
+func CompareIt(a, b []int) (int, int) {
+	var scoreA int
+	var scoreB int
+
+	for i := 0; i < 3; i++ {
+		if a[i] > b[i] {
+			scoreA += 1
+		} else if b[i] > a[i] {
+			scoreB += 1
+		}
+	}
+	return scoreA, scoreB
+}
+
+func ReadArrays() ([]int, []int) {
+	a := make([]int, 3)
+	for i := range a {
+		fmt.Scanf("%d", &a[i])
+	}
+	b := make([]int, 3)
+	for i := range b {
+		fmt.Scanf("%d", &b[i])
+	}
+	return a, b
 }
